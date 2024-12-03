85f5e0f1d091206edc9bf65d3ba58f435e6d6e95
Amanda Vieira <amandavr@gmail.com>
2024-12-03 09:40:52 -0300

    feat: init config tailwind
    - config colors
    - config font Inter
    - create component button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,31 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        transparent: "transparent",
+        primary: {
+          200: "#DCE6E7",
+          400: "#004D61",
+          600: "#013644"
+        },
+        secondary: {
+          200: "#E4EDE3",
+          400: "#47A138",
+          600: "#FF5031"
+        },
+        tertiary: {
+          400: "#FF5031",
+          600: "#FF5031"
+        },
+        gray: {
+          200: "#F5F5F5",
+          400: "#CBCBCB",
+          600: "#8B8B8B"
+        },
+        black: "#000000",
+        white: "#FFFFFF"
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
       },
     },
   },
