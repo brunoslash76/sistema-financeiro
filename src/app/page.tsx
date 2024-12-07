a26170c2c2cefaced0059a16b962e6f06496f69c
Amanda Vieira <amandavr@gmail.com>
2024-12-07 16:29:54 -0300

    feat: structure page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Extract } from '@/components/organisms/extract/Extract';
 import { MainHeader } from '@/components/organisms/main-header/MainHeader';
 import { Nav } from '@/components/organisms/nav/Nav';
 
@@ -5,7 +6,11 @@ export default function Home() {
   return (
    <main className="h-full">
     <MainHeader />
-    <Nav />
+    <div className="px-6 py-8 lg:px-36 lg:py-8 md:px-14 md:py-8 grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <Nav />
+      <div>conteúdo</div>
+      <Extract />
+    </div>
    </main>
-  );
+  )
 }
