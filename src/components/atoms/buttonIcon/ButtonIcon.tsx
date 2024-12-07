65de4084d681e555196d0703143ae3d138a141fd
Amanda Vieira <amandavr@gmail.com>
2024-12-07 15:24:28 -0300

    feat: add icon button

---
+++ src/components/atoms/buttonIcon/ButtonIcon.tsx
@@ -0,0 +1,19 @@
+
+export function ButtonIcon({icon: Icon}) {
+  return (
+    <button className={`
+      w-[30px]
+      h-[30px]
+      flex
+      items-center
+      justify-center
+      text-white
+      bg-primary-400
+      rounded-full 
+      hover:bg-primary-600
+    `}
+    >
+     <Icon />
+    </button>
+  );
+}
