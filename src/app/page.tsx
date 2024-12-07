80f913733e16a51246cf6a78689939b9adb2aa2e
Amanda Vieira <amandavr@gmail.com>
2024-12-07 16:22:46 -0300

    refactor: font-family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Image from "next/image";
 import Button from "./components/button";
 
@@ -99,5 +100,22 @@ export default function Home() {
         </a>
       </footer>
     </div>
+=======
+import { Extract } from '@/components/organisms/extract/Extract';
+import { MainHeader } from '@/components/organisms/main-header/MainHeader';
+import { Nav } from '@/components/organisms/nav/Nav';
+
+export default function Home() {
+  return (
+   <main className="h-full">
+    <MainHeader />
+
+    <div className="px-6 py-8 lg:px-36 lg:py-8 md:px-14 md:py-8 grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <Nav />
+      <div></div>
+      <Extract />
+    </div>
+   </main>
+>>>>>>> Stashed changes
   );
 }
