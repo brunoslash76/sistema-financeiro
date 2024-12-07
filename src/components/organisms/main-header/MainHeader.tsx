ea64528b9656a77fda68d4f58657ebc2afcb1fba
Amanda Vieira <amandavr@gmail.com>
2024-12-07 11:36:23 -0300

    refector: Header

--- src/components/organisms/main-header/MainHeader.tsx
+++ src/components/organisms/main-header/MainHeader.tsx
@@ -1,12 +1,13 @@
+import { HamburguerIcon } from "@/components/icons/HamburguerIcon";
 import { UserIcon } from "@/components/icons/UserIcon";
 
 export function MainHeader() {
   return (
-    <header className="w-full bg-primary-400 lg:p-6">
+    <header className="w-full bg-primary-400 px-6 py-8 lg:px-36 lg:py-8 md:px-14 md:py-8">
       <nav className="w-full flex justify-between">
-        <div className="lg:hidden "></div>
-        <div className="lg:ml-auto flex gap-8 ">
-          <p>Joana da Silva Oliveira</p>
+        <div className="md:hidden"><HamburguerIcon/></div>
+        <div className="md:ml-auto flex gap-8 ">
+          <p className="hidden text-white md:block">Joana da Silva Oliveira</p>
           <UserIcon />
         </div>
       </nav>
