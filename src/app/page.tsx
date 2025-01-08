c4896b14a300262b1cfc4bd7179cf736e88287e6
Pedro Moraes <pedromoraes005@gmail.com>
2025-01-08 00:28:26 -0300

    criando card de saldo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import { Extract } from '@/components/organisms/extract/Extract';
-import { MainHeader } from '@/components/organisms/main-header/MainHeader';
-import { Nav } from '@/components/organisms/nav/Nav';
+import { Balance } from "@/components/organisms/balance/Balance";
+import { Extract } from "@/components/organisms/extract/Extract";
+import { MainHeader } from "@/components/organisms/main-header/MainHeader";
+import { Nav } from "@/components/organisms/nav/Nav";
 
 export default function Home() {
   return (
-   <main className="h-full">
-    <MainHeader />
-    <div className="px-6 py-8 lg:px-36 lg:py-8 md:px-14 md:py-8 grid grid-cols-1 lg:grid-cols-3 gap-6">
-      <Nav />
-      <div>conteúdo</div>
-      <Extract />
-    </div>
-   </main>
-  )
+    <main className="h-full">
+      <MainHeader />
+      <div className="px-6 py-8 lg:px-36 lg:py-8 md:px-14 md:py-8 grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <Nav />
+        <Balance />
+        <Extract />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/organisms/balance/Balance.tsx
@@ -0,0 +1,37 @@
+import { EyeIcon } from "@/components/icons/EyeIcon";
+
+export function Balance() {
+  return (
+    <div
+      id="balance"
+      className="bg-primary-400
+                        flex 
+                        w-full
+                        rounded-lg
+                        py-6
+                        px-6
+                        h-[400px]
+                        w-[690PX]"
+    >
+      <div className="w-1/2 h-full">
+        <h1 className="text-h1 font-bold mb-6">{"Olá, Joana! :)"}</h1>
+        Quinta feira, 09/09/2024
+      </div>
+      <div className="w-1/2 py-8 px-8 h-full flex-col flex items-center justify-center">
+        <div
+          id="eye"
+          className="w-full flex items-center border-b border-white pb-[16px]"
+        >
+          <span className="text-h2 font-bold mr-6">Saldo</span>
+          <EyeIcon />
+        </div>
+        <div id="conta" className="pt-[16px] self-start">
+          Conta corrente
+        </div>
+        <div id="valor" className="pt-[8px] text-[31px] self-start">
+          R$ 2.500,00
+        </div>
+      </div>
+    </div>
+  );
+}
