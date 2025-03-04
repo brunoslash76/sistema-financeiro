4557449fb28252d9dcbac57e8e21fabd1c1c1777
Bruno Russo <bruno@jumpstartmd.com>
2025-03-04 10:17:59 -0300

    feat: add number formating

---
+++ src/utils/number-formating.ts
@@ -0,0 +1,11 @@
+export function moneyMask(value: string) {
+  if (!value) return ''
+  value = value.replace('.', '').replace(',', '').replace(/\D/g, '')
+
+  const options = { minimumFractionDigits: 2 }
+  const result = new Intl.NumberFormat('pt-BR', options).format(
+    parseFloat(value) / 100
+  )
+
+  return result
+}
